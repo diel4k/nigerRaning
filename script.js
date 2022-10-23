4cb73af9d7ebdd4facf7c95d2d1cdae17652487b
daniel4ik <112887790+diel4k@users.noreply.github.com>
2022-10-23 13:41:12 +0300

    update

--- script.js
+++ script.js
@@ -18,9 +18,18 @@ let chaykaLeft = parseInt(window.getComputedStyle(chayka).getPropertyValue("left
 if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
 alert("game over")
 }
-if (chaykaLeft < 50 && chaykaLeft > 0 && dinoTop >= 140){
+if (chaykaLeft < 50 && chaykaLeft > 0 && dinoTop <= 120){
 alert("game over")
 }
+// if (chaykaLeft < 1000 && chaykaLeft > 0 && dinoTop <= 120){
+// function alert(){
+// if (alert.classList != 'aler')
+// alert.classList.add("aler");
+// setTimeout(function(){
+// alert.classList.remove("aler");
+// }, 5000)
+// }
+// }
 // if (let srow = 0, i < 100, i++,actusLeft < 0){
 // let score = document.getElementById("s");
 // score.innerHTML = srow;
